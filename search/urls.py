108c5c338757b381620579c974bafc2d0552163d
CubiRubick <Jose_Guzman_Villegas@outlook.com>
2022-05-07 05:28:48 -0500

    version_7

--- search/urls.py
+++ search/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from search import views
 
 urlpatterns = [
-    path('clientes/', views.client_list),   
+    path('clientes/', views.client_list), 
+    path('cliente/(?P<pk>\d+)', views.client_detail)  
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- search/views.py
+++ search/views.py
@@ -1,7 +1,9 @@
+from pydoc import cli
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from porfolio.models import cliente
+from search import serializers
 from search.serializers import ClientesSerializer
 
 @api_view(['GET', 'POST'])
@@ -10,8 +12,8 @@ def client_list(request):
     List all code Books, or create a new Book.
     """
     if request.method == 'GET':
-        books = cliente.objects.all()
-        serializer = ClientesSerializer(books, many=True)
+        clientes = cliente.objects.all()
+        serializer = ClientesSerializer(clientes, many=True)
         return Response(serializer.data)
 
     elif request.method == 'POST':
@@ -20,3 +22,28 @@ def client_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def client_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        clientes = cliente.objects.get(pk=pk)
+    except cliente.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ClientesSerializer(clientes)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ClientesSerializer(clientes, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        clientes.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
