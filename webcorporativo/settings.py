73bfb1c30887243789eadbff83143054ba5d83c3
CubiRubick <Jose_Guzman_Villegas@outlook.com>
2022-12-12 14:40:18 -0600

    refactor database

--- webcorporativo/settings.py
+++ webcorporativo/settings.py
@@ -77,14 +77,10 @@ WSGI_APPLICATION = 'webcorporativo.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'pruebatecnica',
-            'USER': 'root',
-            'PASSWORD': 'Qwerty@12345',
-            'HOST': 'localhost',
-            'PORT': '3306',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
